6c269b823088885d26bd13bb9ae1dc2fd77dbcaa
Ritesh <rk3818487@gmail.com>
2023-09-18 15:23:29 +0530

    about my application

---
+++ app/src/main/java/com/pupup/about/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.pupup.about
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val about : TextView = findViewById(R.id.textView)
+    }
+}
